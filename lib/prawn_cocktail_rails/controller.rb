04ada914bb512c9266c6a69d5bbfb469bae542b6
Henrik Nyh <henrik@nyh.se>
2019-06-04 11:40:25 +0200

    Use keyword arguments
    We don't support EOL Rubies.

--- lib/prawn_cocktail_rails/controller.rb
+++ lib/prawn_cocktail_rails/controller.rb
@@ -2,9 +2,7 @@ module PrawnCocktailRails
   module Controller
     private
 
-    def send_pdf(document, opts = {})
-      disposition = opts.fetch(:disposition, "attachment")
-
+    def send_pdf(document, disposition: "attachment")
       send_data(
         document.render,
         type: :pdf,
